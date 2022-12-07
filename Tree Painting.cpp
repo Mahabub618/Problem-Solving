9f0f4904af5789a2a3dad1fec11b017701e7ac53
Mahabub Rahman <45081033+Mahabub618@users.noreply.github.com>
2022-12-07 20:37:14 +0600

    Create Tree Painting.cpp

---
+++ Tree Painting.cpp
@@ -0,0 +1,95 @@
+/**********************/
+/* Mahabub Rahman     */
+/* Cse 10th           */
+/* Comilla University */
+/**********************/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define       ll                  long long int
+#define       veci                 vector<int>
+#define       vecl                 vector<long long int>
+#define       pb                   push_back
+#define       newline cerr << endl
+#define       pii                  pair<int, int>
+#define       F                    first
+#define       S                    second
+#define       nl                   "\n"
+#define       all(v)               v.begin(), v.end()
+///============ CONSTANT ===============///
+#define mx9   1000000007
+#define mx5   200005
+#define inf   1<<30                                           //infinity value
+#define eps   1e-9
+#define mod   mx9
+///=============== Debugging ============================///
+#ifndef ONLINE_JUDGE
+#define       debug(x) cout << #x << " = " << x << endl
+#define       debug2(x, y)             cout << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define       debug3(x, y, z)          cout << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define       debug4(a, b, c, d)       cout << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#endif
+void init_code()
+{
+    ios::sync_with_stdio(false); cin.tie(0);cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif    
+}
+const int N = 2e5+5;
+ll dp[N], sum[N], sz[N], n;
+vector<int> g[N];
+void dfs(int u, int p)
+{
+    sum[u] = 1;
+    sz[u] = 1;
+    for(auto v: g[u])
+    {
+        if(v == p) continue;
+        dfs(v, u);
+        sz[u] += sz[v];
+        sum[u] += sum[v];
+    }
+    sum[u] += sz[u]-1;
+}
+ll ans = 0LL;
+void dfs2(int u, int p, ll p_sum)
+{
+    ll cur_s = 0LL;
+    for(auto v: g[u])
+    {
+        if(v == p) continue;
+        cur_s += sum[v];
+    }
+    dp[u] = cur_s + p_sum + n;
+    ans = max(ans, dp[u]);
+    for(auto v: g[u])
+    {
+        if(v == p) continue;
+        dfs2(v, u, dp[u]-sum[v]-sz[v]);
+    }
+}
+int main()
+{
+    init_code();
+    cin >> n;
+    for(int i=1; i<n; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+    dfs(1, -1);
+    dfs2(1, -1, 0LL);
+    // for(int i=1; i<=n; i++)
+    // {
+    //     debug3(i, sum[i], sz[i]);
+    // }
+    cout << ans << nl;
+     
+return 0;
+}
